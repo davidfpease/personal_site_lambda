d371d7f160f996ba37fbcfbe2e6bcf5663296e5d
David Pease <david.f.pease@gmail.com>
2021-03-30 22:17:01 -0700

    Fix double call of joke API

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 console.log('Loading hello world function');
 const axios = require('axios');
-//console.log(axios);
 
 exports.handler = async (event) => {
   let conversationId = "";
@@ -16,69 +15,165 @@ exports.handler = async (event) => {
     conversationId = event.queryStringParameters.conversationId;
   }
 
-  //var axios = require('axios');
-  var data = JSON.stringify({
-    "type": "chat",
-    "body": "Hi!  I've sent a note to my human.  He'll jump in as soon as possible!  In the meantime would you like to hear a joke?",
-    "buttons": [
-      {
-        "value": "Yes please!",
-        "label": "Yes please!",
-        "type": "reply",
-        "reaction": {
-          "type": "replace",
-          "message": "OK, one second while I think of a good one..."
-        }
+  //if inbound request is from the start of a conversation do the following:
+  if(event.queryStringParameters.newConversation){
+    let driftResponseNew = await new Promise((resolve, reject) => {
+      if (!conversationId === ""){
+        console.log("no conversationID!!!")
+        resolve({
+          statusCode: 400,
+          body: 'No conversationId provided.',
+        })
       }
-    ]
-  });
-
-  var config = {
-    method: 'post',
-    url: 'https://driftapi.com/conversations/3078008979/messages',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer FfhkCAX2p2kYSJ4ku1B4NHJIrIy6yQeH'
-    },
-    data: data
-  };
-//this needs to be asynchronous (see medium article) new Promise....
-  axios(config)
-    .then(function (response) {
-      console.log("From inside Axios");
-      console.log(response);
-      console.log(JSON.stringify(response.data));
+      const data = JSON.stringify({
+        "type": "chat",
+        "body": "Hi!  I've sent a note to my human.  He'll jump in as soon as possible!  In the meantime would you like to hear a joke?",
+        "buttons": [
+          {
+            "value": "Yes please!",
+            "label": "Yes please!",
+            "type": "reply",
+            "reaction": {
+              "type": "replace",
+              "message": "OK, one second while I think of a good one..."
+            }
+          }
+        ]
+      });
+      const config = {
+        method: 'post',
+        url: `https://driftapi.com/conversations/${conversationId}/messages`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OAUTH_TOKEN}`
+        },
+        data: data
+      };
+      axios(config)
+        .then(function (response) {
+          resolve({
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+          })
+        })
+        .catch(function (error) {
+          console.log(error);
+          reject({
+            statusCode: 500,
+            body: "Something is broken.",
+          })
+        });
     })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-
-
-  if (event.queryStringParameters && event.queryStringParameters.city) {
-    console.log("Received city: " + event.queryStringParameters.city);
-    city = event.queryStringParameters.city;
   }
 
-  if (event.headers && event.headers['day']) {
-    console.log("Received day: " + event.headers.day);
-    day = event.headers.day;
+  //if inbound request is from a button click
+  if (event.queryStringParameters.dadJoke && event.httpMethod === 'GET') {
+    let driftResponseJoke = await new Promise((resolve, reject) => {
+      if (!conversationId === "") {
+        console.log("no conversationID!!!")
+        resolve({
+          statusCode: 400,
+          body: 'No conversationId provided.',
+        })
+      }
+      const jokeConfig = {
+        method: 'get',
+        url: 'https://www.icanhazdadjoke.com',
+        headers: {
+          'User-Agent': 'https://davidpease.me',
+          'Accept': 'application/json',
+        }
+      };
+
+      axios(jokeConfig)
+        .then(function (response) {
+          resolve({
+            statusCode: 200,
+            body: response.data,
+          })
+        })
+        .catch(function (error) {
+          console.log(error);
+          reject({
+            statusCode: 500,
+            body: "Something is broken.",
+          })
+        });
+    }).catch(error => console.error(error));  
+
+    console.log("driftResponseJoke");
+    console.log(driftResponseJoke);
+
+    let sendJoke = await new Promise((resolve, reject) => {
+      let joke = driftResponseJoke.body.joke;
+      const data = JSON.stringify({
+        "type": "chat",
+        "body": joke,
+      });
+      const config2 = {
+        method: 'post',
+        url: `https://driftapi.com/conversations/${conversationId}/messages`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OAUTH_TOKEN}`
+        },
+        data: data
+      };
+
+      axios(config2)
+        .then(function (response) {            
+          console.log("Joke sent to API");
+          const data = JSON.stringify({
+            "type": "chat",
+            "body": "Would you like to hear another joke?",
+            "buttons": [
+              {
+                "value": "Yes please!",
+                "label": "Yes please!",
+                "type": "reply",
+                "reaction": {
+                  "type": "replace",
+                  "message": "OK, one second while I think of a good one..."
+                }
+              }
+            ]
+          });
+          const config2 = {
+            method: 'post',
+            url: `https://driftapi.com/conversations/${conversationId}/messages`,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${process.env.OAUTH_TOKEN}`
+            },
+            data: data
+          };
+
+          axios(config2)
+            .then(function (response) {
+              console.log("ASked for more jokes");
+              resolve();
+            })
+            .catch(function (error) {
+              console.log(error);
+              reject();
+            });
+      })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+    }).catch(error => console.error(error));
   }
 
-  if (event.body) {
-    let body = JSON.parse(event.body)
-    if (body.time)
-      time = body.time;
-  }
 
-  let greeting = `Good ${time}, name of ${city}.`;
-  if (day) greeting += ` Happy ${day}!`;
+//handle an inbound joke request
+  let dadJoke = `Good day sir!`;
+  
 
   let responseBody = {
-    message: greeting,
+    message: dadJoke,
     input: event,
-    conversationId: conversationId,
-    oauth: process.env.OAUTH_TOKEN,
+    conversationId: conversationId+"",
   };
 
   // The output from a Lambda proxy integration must be 
@@ -99,3 +194,5 @@ exports.handler = async (event) => {
   console.log("response: " + JSON.stringify(response))
   return response;
 };
+
+            
